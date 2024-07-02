56bb07124bb7b673d3555350495e3a4dbf49fd83
Artem Babii <arthem.babij@gmail.com>
2024-07-02 16:21:50 +0300

    use dynamic icons

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from pathlib import Path
 
 from ulauncher.api import Extension, Result
 from ulauncher.api.shared.action.RunScriptAction import RunScriptAction
+from ulauncher.modes.file_browser.get_icon_from_path import get_icon_from_path
 
 
 ICON='utilities-terminal'
@@ -17,14 +18,14 @@ class Launchpad(Extension):
         scripts_dir = Path(self.preferences['scripts_dir'] or '~').expanduser().resolve()
         if not scripts_dir.is_dir():
             return [Result(icon=ICON,
-                           name='Hmm, the provided directory does not exist',
+                           name='The provided directory does not exist',
                            description=f'Ensure that {scripts_dir} directory exists',
                            on_enter=True)]
 
         scripts = [f for f in scripts_dir.iterdir() if f.is_file()]
         if not scripts:
             return [Result(icon=ICON,
-                           name='No scripts to launch',
+                           name='No scripts found',
                            description=f'Place the scripts into {scripts_dir}',
                            on_enter=True)]
 
@@ -40,12 +41,12 @@ class Launchpad(Extension):
                            on_enter=True)]
 
         items = []
-        for i in matches:
-            mime = magic.detect_from_filename(f'{i.parent}/{i.name}').mime_type
-            items.append(Result(icon=ICON,
+        for i in matches[:25]:
+            path = str(i)
+            items.append(Result(icon=get_icon_from_path(path),
                                 name=i.name,
-                                description=mime,
-                                on_enter=RunScriptAction(f'{i.parent}/{i.name}')))
+                                description=magic.detect_from_filename(path).mime_type,
+                                on_enter=RunScriptAction(path)))
         return items
 
 
